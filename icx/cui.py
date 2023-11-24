9e30e93acd2f593738776277d135739e26ff694b
MoonKyu Song <lanterrt@gmail.com>
2023-11-24 11:55:09 +0900

    Support wrapping in MapPrinter

--- icx/cui.py
+++ icx/cui.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python3
 
 import sys
-from typing import List
+import textwrap
+from typing import List, Optional
 
 import click
 
+
+def get_align_of(s: Optional[str]) -> str:
+    if s is None:
+        return '<'
+    if '>' in s:
+        return '>'
+    if '^' in s:
+        return '^'
+    return '<'
 
 class Column:
     def __init__(self, get_value, size: int, format: str = None, name: str = '') -> None:
         self.__get_value = get_value
         size = max(size, len(name))
         self.__size = size
-        if format is None:
-            self.__format = f'{{:{size}.{size}}}'
-        elif '>' in format:
-            self.__format = f'{{:>{size}.{size}}}'
-        elif '^' in format:
-            self.__format = f'{{:^{size}.{size}}}'
+        align = get_align_of(format)
+        if size == 0:
+            self.__format = '{}'
         else:
-            self.__format = f'{{:<{size}.{size}}}'
+            self.__format = f'{{:{align}{size}.{size}}}'
         self.__value_format = format
         self.__name = name
 
+    def get_align(self) -> str:
+        return get_align_of(self.__value_format)
+
     def get_value(self, *args) -> any:
-        return self.__get_value(*args)
+        if callable(self.__get_value):
+            return self.__get_value(*args)
+        else:
+            return self.__get_value
 
     def get_str(self, *args) -> str:
         if self.__value_format is not None:
-            return self.__value_format.format(self.__get_value(*args))
+            value = self.get_value(*args)
+            if isinstance(value, tuple):
+                return self.__value_format.format(*value)
+            else:
+                return self.__value_format.format(value)
         else:
-            return self.__get_value(*args)
+            return self.get_value(*args)
 
     @property
     def size(self):
@@ -125,17 +142,23 @@ class MapPrinter:
                 max_value = max(max_value, row.size)
 
         max_width = max(max_title, max_value+max_name+3)
-        if max_width > max_title:
-            max_value = max_width - (max_name+3)
+        max_value = max_width - (max_name+3)
 
         self.__rows = rows
-        self.__format_title = '| '+f'{{:^{max_width}}}' + ' |'
+        self.__max_width = max_width
+        self.__max_name = max_name
+        self.__max_value = max_value
         self.__sep_str = '+-' + '-'*max_name + '-+-' + '-'*max_value + '-+'
-        self.__format_left = '| ' + f'{{:>{max_name}}}' + ' | ' + f'{{:<{max_value}}}' + ' |'
-        self.__format_right = '| ' + f'{{:>{max_name}}}' + ' | ' + f'{{:>{max_value}}}' + ' |'
-        self.__format_center = '| ' + f'{{:^{max_name}}}' + ' | ' + f'{{:^{max_value}}}' + ' |'
-        self.__header = self.__format_center.format('Name', 'Value')
+        self.__header = self.__name_value_format('^').format('Name', 'Value')
         self.__file = file
+
+    def __title_format(self, align) -> str:
+        return f'| {{:{align}{self.__max_width}.{self.__max_width}}} |'
+
+    def __name_value_format(self, align) -> str:
+        name_len = self.__max_name
+        value_len = self.__max_value
+        return f'| {{:>{name_len}.{name_len}}} | {{:{align}{value_len}.{value_len}}} |'
 
     def print_header(self, **kwargs) -> 'MapPrinter':
         click.secho(self.__header, reverse=True, file=self.__file, **kwargs)
@@ -148,26 +171,29 @@ class MapPrinter:
     def print_data(self, *args, **kwargs) -> 'MapPrinter':
         for row in self.__rows:
             value = row.get_str(*args)
-            row_format = row.format
-            if '>' in row_format:
-                format_str = self.__format_right
-                value_str = row_format.format(value)[-row.size:]
-            elif '^' in row_format:
-                format_str = self.__format_center
-                value_str = row_format.format(value)[:row.size]
-            else:
-                format_str = self.__format_left
-                value_str = row_format.format(value)[:row.size]
+            align = row.get_align()
 
             if isinstance(row, Header):
+                format_str = self.__title_format(align)
                 click.secho(
-                    self.__format_title.format(value_str),
+                    format_str.format(value),
                     file = self.__file,
                     reverse=True,
                     **kwargs)
             else:
-                click.secho(
-                    format_str.format(row.name, value_str),
-                    file = self.__file,
-                    **kwargs)
+                format_str = self.__name_value_format(align)
+
+                name = row.name
+                lines = textwrap.wrap(value, self.__max_value)
+                while len(lines) > 0:
+                    line = lines.pop(0)
+                    kwargs_line = dict(**kwargs)
+                    if len(lines) > 0 and 'underline' in kwargs_line:
+                        kwargs_line.pop('underline')
+
+                    click.secho(
+                        format_str.format(name, line),
+                        file = self.__file,
+                        **kwargs_line)
+                    name = ''
         return self
