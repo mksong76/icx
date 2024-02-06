ed4fd72d052de488ab0106709dd80c91bc63b600
MoonKyu Song <lanterrt@gmail.com>
2024-02-06 14:00:09 +0900

    Show bonds/unbonds for icx icon account

--- icx/icon/basic.py
+++ icx/icon/basic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from datetime import timedelta
 import click
 
 
@@ -25,14 +26,55 @@ def show_account(addr: str):
             Row(util.format_decimals(claimable, 3), 16, '{:>12s} ICX', 'Claimable')
         ]
 
-    staked, unstaking, _ = sum_stake(asset.get_stake(addr))
+    stake_info = asset.get_stake(addr)
+    staked, unstaking, _ = sum_stake(stake_info)
+
     if staked > 0 or unstaking > 0:
+        bond_info = asset.get_bond(addr)
+        total_bonded = int(bond_info['totalBonded'], 0)
         rows += [
             Row(lambda v: util.format_decimals(staked, 3),
                 24, '{:>20s} ICX', 'Stake'),
             Row(lambda v: util.format_decimals(unstaking, 3),
                 24, '{:>20s} ICX', 'Unstaking'),
+            Row(lambda v: util.format_decimals(total_bonded, 3),
+                24, '{:>20s} ICX', 'Bonded'),
         ]
+        idx = 0
+        for unstake in stake_info.get('unstakes', []):
+            unstake_amount = int(unstake.get('unstake', '0x0'), 0)
+            unstake_height = int(unstake.get('unstakeBlockHeight', '0x0'), 0)
+            remains = timedelta(seconds=int(unstake.get('remainingBlocks', '0x0'), 0)*2)
+            rows += [
+                Header(f'Unstake[{idx}]', 0),
+                Row(util.format_decimals(unstake_amount, 3),
+                    24, '{:>20s} ICX', 'Amount'),
+                Row( f'Height: {unstake_height} ( {remains} )',
+                    26, '{:>26s}', 'Expires'),
+            ]
+            idx += 1
+
+        idx = 0
+        for bond in bond_info.get('bonds', []):
+            rows += [
+                Header(f'Bond[{idx}]', 0),
+                Row(bond['address'], 20, '{:<20s}', 'Address'),
+                Row(util.format_decimals(bond['value'], 3), 20, '{:<20s}', 'Amount'),
+            ]
+            idx += 1
+
+        last_height = asset.get_last_height()
+        idx = 0
+        for unbond in bond_info.get('unbonds', []):
+            unbond_height = int(unbond.get('expireBlockHeight', '0x0'), 0)
+            unbond_remain = timedelta(seconds=(unbond_height - last_height)*2)
+            rows += [
+                Header(f'Unbond[{idx}]', 0),
+                Row(unbond['address'], 20, '{:<20s}', 'Address'),
+                Row(util.format_decimals(unbond['value'], 3), 24, '{:>20s} ICX', 'Amount'),
+                Row(f'Height: {unbond_height} ( {unbond_remain} )', 26, '{:>26s}', 'Expires'),
+            ]
+            idx += 1
 
     if not addr.startswith('cx'):
         try:
